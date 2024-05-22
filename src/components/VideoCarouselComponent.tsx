b5ce45edd47f1d41fd898e8a9b1336d75ca163cb
Nguyen Le Huu Hoang <contact.hoangng26@gmail.com>
2024-05-22 18:45:14 +0700

    feat: build video carousel component and add landscape poster resources

--- src/components/VideoCarouselComponent.tsx
+++ src/components/VideoCarouselComponent.tsx
@@ -1,11 +1,77 @@
-import React from 'react';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { CustomArrowProps } from '@ant-design/react-slick';
+import { Carousel, CarouselProps } from 'antd';
+import React, { PropsWithChildren } from 'react';
 
-const VideoCarouselComponent: React.FC = () => {
+interface VideoCarouselProps {
+  title: string;
+  slideToShow?: number;
+}
+
+const VideoCarouselComponent: React.FC<PropsWithChildren<VideoCarouselProps>> = ({ children, title, slideToShow }) => {
+  const settings: CarouselProps = {
+    slidesToShow: slideToShow || 3,
+    slidesToScroll: 3,
+    arrows: true,
+    dots: false,
+    infinite: false,
+    draggable: true,
+    swipe: true,
+    swipeToSlide: true,
+    prevArrow: <CarouselArrowLeft />,
+    nextArrow: <CarouselArrowRight />,
+  };
+
   return (
     <div className="py-8">
-      <h2 className="font-bold text-2xl">Latest Released</h2>
+      <h2 className="font-bold text-2xl mb-4">{title}</h2>
+      <Carousel {...settings}>{children}</Carousel>
     </div>
   );
 };
 
 export default VideoCarouselComponent;
+
+const CarouselArrowLeft: React.FC<CustomArrowProps> = ({ currentSlide, slideCount, ...props }) => {
+  if (!slideCount) {
+    return;
+  }
+
+  return (
+    <button
+      {...props}
+      className={`slick-prev slick-arrow${
+        currentSlide === 0 ? ' slick-disabled ' : ' '
+      }after:content-none w-fit h-fit text-3xl`}
+      aria-hidden="true"
+      aria-disabled={currentSlide === 0 ? true : false}
+      type="button"
+    >
+      <span className="hover:bg-gradient-to-l hover:from-transparent hover:from-0% hover:to-orange-custom hover:to-[500%] rounded-md p-2">
+        <LeftOutlined />
+      </span>
+    </button>
+  );
+};
+
+const CarouselArrowRight: React.FC<CustomArrowProps> = ({ currentSlide, slideCount, ...props }) => {
+  if (!slideCount) {
+    return;
+  }
+
+  return (
+    <button
+      {...props}
+      className={`slick-next slick-arrow${
+        currentSlide === slideCount - 1 ? ' slick-disabled ' : ' '
+      }after:content-none w-fit h-fit text-3xl`}
+      aria-hidden="true"
+      aria-disabled={currentSlide === slideCount - 1 ? true : false}
+      type="button"
+    >
+      <span className="hover:bg-gradient-to-r hover:from-transparent hover:from-0% hover:to-orange-custom hover:to-[500%] rounded-md p-2">
+        <RightOutlined />
+      </span>
+    </button>
+  );
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,189 @@ const HomePage: React.FC = () => {
   return (
     <>
       <BannerCarouselComponent />
-      <VideoCarouselComponent />
+      <VideoCarouselComponent title="Latest Released">
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/doraemon_landscape.jpg" alt="tear-of-steels_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Doraemon the Movie: Nobita's Little Star Wars (2022)
+          </p>
+        </div>
+
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/tear-of-steels_landscape.png" alt="tear-of-steels_landscape" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">Tear of Steel</p>
+        </div>
+
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/tinoaa-landscape.jpg" alt="tinoaa-landscape" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">There's No One At All</p>
+        </div>
+
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/worlds-23_landscape.jpg" alt="worlds-23_landscape" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Final: T1 vs Weibo Gaming
+          </p>
+        </div>
+      </VideoCarouselComponent>
+
+      <VideoCarouselComponent title="Trending" slideToShow={5}>
+        <div>
+          <div className="h-[27vh] rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img className="h-full w-full object-cover" src="/doraemon.jpg" alt="doraemon_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Doraemon the Movie: Nobita's Little Star Wars (2022)
+          </p>
+        </div>
+
+        <div>
+          <div className="h-[27vh] rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img className="h-full w-full object-cover" src="/doraemonvn.jpg" alt="doraemon_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Doraemon the Movie: Nobita's Little Star Wars (2022)
+          </p>
+        </div>
+
+        <div>
+          <div className="h-[27vh] rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img className="h-full w-full object-cover" src="/tear-of-steels.png" alt="tear-of-steels_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">Tear of Steel</p>
+        </div>
+
+        <div>
+          <div className="h-[27vh] rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img className="h-full w-full object-cover" src="/tinoaa.jpg" alt="tinoaa_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">There's No One At All</p>
+        </div>
+
+        <div>
+          <div className="h-[27vh] rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img className="h-full w-full object-cover" src="/worlds-23.jpg" alt="worlds-23_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Final: T1 vs Weibo Gaming
+          </p>
+        </div>
+
+        <div>
+          <div className="h-[27vh] rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img className="h-full w-full object-cover" src="/doraemon.jpg" alt="doraemon_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Doraemon the Movie: Nobita's Little Star Wars (2022)
+          </p>
+        </div>
+
+        <div>
+          <div className="h-[27vh] rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img className="h-full w-full object-cover" src="/doraemonvn.jpg" alt="doraemon_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Doraemon the Movie: Nobita's Little Star Wars (2022)
+          </p>
+        </div>
+
+        <div>
+          <div className="h-[27vh] rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img className="h-full w-full object-cover" src="/tear-of-steels.png" alt="tear-of-steels_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">Tear of Steel</p>
+        </div>
+
+        <div>
+          <div className="h-[27vh] rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img className="h-full w-full object-cover" src="/tinoaa.jpg" alt="tinoaa_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">There's No One At All</p>
+        </div>
+
+        <div>
+          <div className="h-[27vh] rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img className="h-full w-full object-cover" src="/worlds-23.jpg" alt="worlds-23_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Final: T1 vs Weibo Gaming
+          </p>
+        </div>
+      </VideoCarouselComponent>
+
+      <VideoCarouselComponent title="Top 10 Drama" slideToShow={5}>
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/doraemon_landscape.jpg" alt="tear-of-steels_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Doraemon the Movie: Nobita's Little Star Wars (2022)
+          </p>
+        </div>
+
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/tear-of-steels_landscape.png" alt="tear-of-steels_landscape" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">Tear of Steel</p>
+        </div>
+
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/tinoaa-landscape.jpg" alt="tinoaa-landscape" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">There's No One At All</p>
+        </div>
+
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/worlds-23_landscape.jpg" alt="worlds-23_landscape" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Final: T1 vs Weibo Gaming
+          </p>
+        </div>
+
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/doraemon_landscape.jpg" alt="tear-of-steels_alt" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Doraemon the Movie: Nobita's Little Star Wars (2022)
+          </p>
+        </div>
+
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/tear-of-steels_landscape.png" alt="tear-of-steels_landscape" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">Tear of Steel</p>
+        </div>
+
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/tinoaa-landscape.jpg" alt="tinoaa-landscape" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">There's No One At All</p>
+        </div>
+
+        <div>
+          <div className="rounded-xl overflow-hidden scale-[98%] shadow-custom outline-none">
+            <img src="/worlds-23_landscape.jpg" alt="worlds-23_landscape" />
+          </div>
+          <p className="w-11/12 translate-x-[1%] font-semibold text-base mt-4 line-clamp-1">
+            Final: T1 vs Weibo Gaming
+          </p>
+        </div>
+      </VideoCarouselComponent>
     </>
   );
 };
