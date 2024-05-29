2fc36ccad0c1540eeed8a1fca0f0d84443063c88
Nguyen Le Huu Hoang <contact.hoangng26@gmail.com>
2024-05-29 17:01:07 +0700

    fix: header is behind video player in WatchPage

--- src/components/HeaderComponent/index.tsx
+++ src/components/HeaderComponent/index.tsx
@@ -7,7 +7,7 @@ import UserActionComponent from './UserActionComponent';
 
 const HeaderComponent: React.FC = () => {
   return (
-    <Header className="px-2 py-4 h-20 sticky top-0 z-[1] bg-transparent backdrop-blur-3xl rounded">
+    <Header className="px-2 py-4 h-20 sticky top-0 z-50 bg-transparent backdrop-blur-3xl rounded">
       <div className="h-full flex justify-between items-center">
         <Button size="large" className="border-none bg-transparent shadow-none p-0 h-full">
           <Link to="/">
